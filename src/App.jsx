d1cf442b98b6e8822e5f27e5684ae3696acd4f65
Prince9g <142648202+Prince9g@users.noreply.github.com>
2024-12-04 23:07:03 +0530

    added some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import React, { useContext } from 'react'
 import image from './assets/ai.png';
+import speakImg from './assets/speak.gif'
+import aigif from './assets/aiVoice.gif'
 import { CiMicrophoneOn } from "react-icons/ci";
 import { datacontext } from './context/UserContext';
 const App = () => {
-  let {recognition} = useContext(datacontext);
+  let {recognition, speaking, setSpeaking, texting, setTexting, response, setResponse} = useContext(datacontext);
   // speak("hello prince how are you dood");
   return (
     <div className="main">
       <img src={image} alt="" id="assistant"/>
       <span>Hi! I'm your Virtual Assitant Sir</span>
-      <button onClick={() =>{
+      {!speaking ? <button onClick={() =>{
+        setTexting("Listening...")
+        setSpeaking(true)
+        setResponse(false)
         recognition.start()
       }}>Click here <CiMicrophoneOn />
-      </button>
+      </button> 
+      : 
+      <div className="response">
+        {!response ? <img src={speakImg} alt="" id="speak"/> 
+      :   <img src={aigif} alt="" id="aigif"/>
+      }
+        
+        <p>{texting}</p>
+      </div>
+       }
+      
     </div>
   )
 }
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,9 +1,11 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import run from "../API";
 export const datacontext = createContext(null);
 
 const UserContext = ({ children }) => {
-  // let [speaking, setSpeaking]
+  let [speaking, setSpeaking] = useState(false);
+  let [texting, setTexting] = useState("Listening...");
+  let [response, setResponse] = useState(false);
     const speak = (text) =>{
         // speech synthesis to convert text to speech (js inbuilt free class)
         window.speechSynthesis.cancel()
@@ -16,7 +18,13 @@ const UserContext = ({ children }) => {
     }
     const aiResponse = async(prompt) =>{
         let text = await run(prompt);
-        speak(text);
+        let newText = text.split("**")&&text.split("*")&&text.replace("google", "Prince Sharma")&&text.replace("Google", "Prince Sharma")
+        setTexting(newText);
+        speak(newText);
+        setResponse(true);
+        setTimeout(()=>{
+          setSpeaking(false)
+        }, 5000)
     }
     // now speech recognition for recognize what user said to our virtual assitant(js inbuilt free function)
     let speechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition
@@ -25,10 +33,78 @@ const UserContext = ({ children }) => {
         let currentIndex = e.resultIndex;
         let transcript = e.results[currentIndex][0].transcript
         console.log(transcript)
-        aiResponse(transcript)
+        setTexting(transcript)
+        takeCommand(transcript.toLowerCase())
+        // aiResponse(transcript)
     }
+
+    const takeCommand = (command) => {
+      if(command.includes("open") && command.includes("youtube")){
+        window.open("https://www.youtube.com/", "_blank")
+        speak("opening Youtube")
+        setResponse(true);
+        setTexting("opening Youtube...")
+        setTimeout(()=>{
+          setSpeaking(false)
+        }, 5000)
+      }else if(command.includes("open") && command.includes("google")){
+        window.open("https://www.google.com/", "_blank")
+        speak("opening google")
+        setResponse(true);
+        setTexting("opening Google...")
+        setTimeout(()=>{
+          setSpeaking(false)
+        }, 5000)
+      }else if(command.includes("open") && command.includes("instagram")){
+        window.open("https://www.instagram.com/", "_blank")
+        speak("opening instagram")
+        setResponse(true);
+        setTexting("opening Instagram...")
+        setTimeout(()=>{
+          setSpeaking(false)
+        }, 5000)
+      }else if(command.includes("open") && command.includes("leetcode")){
+        window.open("https://leetcode.com/problemset/", "_blank")
+        speak("opening Leetcode")
+        setResponse(true);
+        setTexting("opening Leetcode...")
+        setTimeout(()=>{
+          setSpeaking(false)
+        }, 5000)
+      }else if(command.includes("time")){
+        let time = new Date().toLocaleString(undefined, 
+          {hour:"numeric", minute:"numeric"}
+        )
+        speak(time);
+        setTexting(time)
+        setResponse(true);
+        setTimeout(()=>{
+          setSpeaking(false)
+        }, 5000)
+      }else if(command.includes("date")){
+        let date = new Date().toLocaleString(undefined,
+          {day:"numeric", month:"short"}
+        )
+        speak(date);
+        setTexting(date)
+        setResponse(true);
+        setTimeout(()=>{
+          setSpeaking(false)
+        }, 5000)
+      }
+        else{
+        aiResponse(command)
+      }
+    }
+
     let value={
-        recognition
+        recognition, 
+        speaking, 
+        setSpeaking,
+        texting,
+        setTexting,
+        response,
+        setResponse
     }
   return (
     <div>
